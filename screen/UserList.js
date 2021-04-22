63d4dfe1bc2210967329b7c3ea0d871db46dd0c9
rosarioconde <conde.rosario88@gmail.com>
2021-04-21 21:29:01 -0300

    react-native-elements - avatar ListItem users

--- screen/UserList.js
+++ screen/UserList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView, Button } from "react-native";
 import firebase from "../database/firebase";
+import { ListItem, Avatar } from "react-native-elements";
 const UserList = (props) => {
     //al inicio de la aplicacion no va a tener ningun usuario
     const [users, setUsers] = useState([])
@@ -8,7 +9,7 @@ const UserList = (props) => {
     //llamar a la ejecucion de firebase
     useEffect(() => {
         firebase.db.collection('users').onSnapshot(querySnapshot => {
-            const user = [];
+            const users = [];
             querySnapshot.docs.forEach((doc) => {
                 const { name, email, cel } = doc.data()
                 //console.log(doc.data())
@@ -25,7 +26,24 @@ const UserList = (props) => {
     return (
         <ScrollView>
             <Button title="crear usuario" onPress={() => props.navigation.navigate('CreateUser')} />
+            {
+                users.map(user => {
+                    return (
+                        <ListItem key={user.id}>
+                            <ListItem.Chevron />
+                            <Avatar source={{ 
+                                uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg', }}
+                                rounded />
+                            <ListItem.Content>
+                                <ListItem.Title>{user.name}</ListItem.Title>
+                                <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                            </ListItem.Content>
+                        </ListItem>
+                    );
+                })
+            }
         </ScrollView>
+        //recorrer lista de usuarios
 
     )
 }
